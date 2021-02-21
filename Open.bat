e3f3389c1976cdd4602cf51ff142b689d46e365b
Rohit Ramtirthe <49193132+Rohit-MyRG@users.noreply.github.com>
2021-02-21 20:47:05 +0530

    Added Open.bat and Exel File

---
+++ Open.bat
@@ -0,0 +1,14 @@
+@echo off 
+color 0a
+echo Enter The password :
+set /p ans=
+if %ans%==Password goto open
+echo Sorry Wrong password
+pause
+exit
+
+:open
+H:
+cd dir1\dir2\dir3
+start FileName.xlsx
+exit
